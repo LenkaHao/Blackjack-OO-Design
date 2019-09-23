eed01a048100b8114a80115ae2bc3ed311219ea3
LenkaHao <jthao@bu.edu>
2019-09-23 14:58:05 -0400

    initialize directory

---
+++ src/Card.java
@@ -0,0 +1,17 @@
+public class Card {
+    private String suit;
+    private String value;
+
+    public Card(String suit, String value) {
+        this.suit = suit;
+        this.value = value;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
