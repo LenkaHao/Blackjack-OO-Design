7dcf5ea5f9df3e7162298772cfb04836983b8419
Lijun <lijunc.mi@gmail.com>
2019-09-29 20:27:46 -0400

    Update GameVisualizer.java

--- src/GameVisualizer.java
+++ src/GameVisualizer.java
@@ -13,16 +13,20 @@ public class GameVisualizer {
         System.out.println("Created by Jiatong Hao, Xiankang Wu and Lijun Chen on 9/23/2019.\n");
     }
 
-    public void invalidAction(String action) {
-        System.out.println("You cannot " + action + ". Please select another action.");
-    }
-
     public void displayInfo(int playerId, int playerBalance, int handIdx) {
         System.out.println("Player: " + playerId + ", Balance = " + playerBalance + ", Hand: " + handIdx);
     }
 
     public void displayPlayerHand(int handIdx, BlackjackHand hand) {
-        System.out.println("Your hand " + handIdx + " is:\n" + hand);
+        System.out.println("Your current hand " + handIdx + " is:\n" + hand);
+    }
+
+    public void displayDealerHand(BlackjackHand hand) {
+        System.out.println("Dealer's current hand is:\n" + hand);
+    }
+
+    public void invalidAction(String action) {
+        System.out.println("You cannot " + action + ". Please select another action.");
     }
 
     public void printPlayerBalance(int playerId, int roundBalance, int playerBalance, int roundNum) {
